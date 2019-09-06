290bb32f7a752b0ede19d09cbd4ab19cdef14675
Skyware-developer <gabriel.bateca@outlook.com>
2019-09-06 00:31:06 -0500

    Cambios de identación

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 /**VARIABLES QUE SE VAN A USAR PARA LA TOMA DE DATOS */
 var btnAbrirPopup = document.getElementById('sign-in'),
     overlay = document.getElementById('overlay'),
-        popup = document.getElementById('popup'),
-        btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 /**SE CREA FUNCION PARA EL BOTON ABRIR POPUP */
-btnAbrirPopup.addEventListener('click', function(){
+btnAbrirPopup.addEventListener('click', function () {
     overlay.classList.add('active');/**pone al ovelay en modo activo */
     popup.classList.add('active');/**pone el popup en modo activo */
 });
 /**SE CREA LA FUNCION PARA EL BOTON CERRAR */
-btnCerrarPopup.addEventListener('click', function(){
+btnCerrarPopup.addEventListener('click', function () {
     overlay.classList.remove('active'); /**quita el modo activo de overlay */
     popup.classList.remove('active');/**quita el modo activo del popup */
 });

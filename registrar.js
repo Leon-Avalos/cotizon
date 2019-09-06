0ef8f8487e8b82001e707c91d6804cf8b45c38ca
Skyware-Developer <gabriel.bateca@outlook.com>
2019-09-06 12:53:12 -0500

    Se agregó la opción de registrar a un usuario

---
+++ registrar.js
@@ -0,0 +1,32 @@
+function registrarUsuario() {
+    var nombre = document.getElementById('regNombre').value;
+    var correo = document.getElementById('regCorreo').value;
+    var clave = document.getElementById('regPass').value;
+    var confirmar_clave = document.getElementById('regPass2').value;
+
+    if (nombre != '' && correo != '') {
+        if (!(clave === confirmar_clave)) {
+            alert('Las claves ingresadas no coinciden.')
+        } else {
+            var usuario = {
+                name: nombre,
+                email: correo,
+                password: clave
+            };
+
+            localStorage.setItem(usuario.name, JSON.stringify(usuario));
+            alert('Ha sido registrado exitosamente.')
+            console.log('retrievedObject: ', JSON.parse(localStorage.getItem(usuario.name)));
+            limpiarCampos();
+        }        
+    } else {
+        alert('Por favor no deje campos en vacío.')
+    }
+}
+
+function limpiarCampos() {
+    document.getElementById('regNombre').value = '';
+    document.getElementById('regCorreo').value = ''
+    document.getElementById('regPass').value = ''
+    document.getElementById('regPass2').value = ''
+}
